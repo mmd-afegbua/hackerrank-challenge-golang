a3d95ce361cb21fc478ff455b6565d8d00401e21
mmd-afegbua <afegbuaismaila@gmail.com>
2020-06-04 08:52:15 +0100

    day 5 with no comment

---
+++ day5.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+    checkError(err)
+	n := int(nTemp)
+
+	for i := 0; i <= 10; i++ {
+		if i == 0 {
+			continue
+		} else if i >= 1 {
+			fmt.Printf("%v x %v = %v\n", n, i, n*i)
+		}
+	}
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
