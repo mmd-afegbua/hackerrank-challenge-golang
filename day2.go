2255cc553e53f878ac4ebc7d13ba34cb99a40f5c
mmd-afegbua <afegbuaismaila@gmail.com>
2020-06-14 20:20:00 +0100

    finally, a comment!

--- day2.go
+++ day2.go
@@ -9,14 +9,15 @@ import (
 	"strings"
 )
 
+//  dataInput returns a Reader
 var dataInput = bufio.NewReaderSize(os.Stdin, 1024 * 1024)
 
 func main() {
-
+	// first Reader input
 	mealCost, _ := strconv.ParseFloat(inputLine(), 64)
-
+	// second Reader input
 	tipPercent, _ := strconv.ParseFloat(inputLine(), 64)
-
+	// third Reader input
 	taxPercent, _ := strconv.ParseFloat(inputLine(), 64)
 
 	n := calc(mealCost, tipPercent, taxPercent)
@@ -32,8 +33,12 @@ func calc(mealCost float64, tipPercent float64, taxPercent float64) float64 {
 
 }
 
+// function to read string into a variable
 func inputLine() string {
+	// input returns a string from ReadString function
+	// the '\n' signifies that Enter will end the input
 	input, _ := dataInput.ReadString('\n')
+	// TrimSuffix returns a given string without the provided suffix
 	input = strings.TrimSuffix(input, "\n")
 	return input
 }
