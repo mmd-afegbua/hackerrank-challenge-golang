b3350527944b0d7bb759a4096e5de2be93cb0544
mmd-afegbua <afegbuaismaila@gmail.com>
2020-06-16 21:36:12 +0100

    added day10 and day11

--- day10.go
+++ day10.go
@@ -2,19 +2,46 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"strconv"
+	"strings"
+	"os"
+	"bufio"
+	"io"
 )
+
+func main() {
+	inputLine := bufio.NewReader(os.Stdin)
+	input := lineReader(inputLine)
+	num, err := strconv.ParseInt(input, 10, 64)
+	checkError(err)
+
+	binNum := strconv.FormatInt(num, 2)
+	binArray := strings.Split(binNum, "0")
+	// max is float64 type
+	var count float64
+
+	for _, value := range binArray {
+		splitNum, _ := strconv.ParseFloat(value, 64)
+		count = math.Max(splitNum, count)
+	}
+
+	consecs := strconv.Itoa(int(count))
+	fmt.Println(len(consecs))
+
+}
 
 
 func lineReader(reader *bufio.Reader) string {
-	str, _, err = reader.ReadLine()
+	str, _, err := reader.ReadLine()
 	if err == io.EOF {
 		return " "
 	}
-	return strings.TrimRight(string(str), '\r\n')
+	return strings.TrimRight(string(str), "\r\n")
 }
 
 func checkError(err error) {
-	if err =! nil {
+	if err != nil {
 		panic(err)
 	}
 }
